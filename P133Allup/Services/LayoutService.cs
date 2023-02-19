2466b0c4ef599a24ec8fba8a2b95574c893b4e59
SamadAliyev20 <samadaa@code.edu.az>
2023-02-20 00:54:26 +0400

    Add project files.

---
+++ P133Allup/Services/LayoutService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using P133Allup.DataAccessLayer;
+using P133Allup.Interfaces;
+using P133Allup.Models;
+using P133Allup.ViewModels.BasketViewModels;
+
+namespace P133Allup.Services
+{
+    public class LayoutService : ILayoutService
+    {
+        private readonly AppDbContext _context;
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public LayoutService(AppDbContext context, IHttpContextAccessor contextAccessor)
+        {
+            _context = context;
+            _contextAccessor = contextAccessor;
+        }
+
+        public async Task<IEnumerable<BasketVM>> GetBaskets()
+        {
+            string basket = _contextAccessor.HttpContext.Request.Cookies["basket"];
+
+            List<BasketVM> basketVMs = null;
+
+            if (!string.IsNullOrWhiteSpace(basket))
+            {
+                basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(basket);
+
+                foreach (BasketVM basketVM in basketVMs)
+                {
+                    Product product = await _context.Products.FirstOrDefaultAsync(p=>p.Id== basketVM.Id && p.IsDeleted==false);
+
+                    if (product != null)
+                    {
+                        basketVM.ExTax = product.ExTax;
+                        basketVM.Price = product.DiscountedPrice > 0 ? product.DiscountedPrice : product.Price;
+                        basketVM.Title= product.Title;
+                        basketVM.Image = product.MainImage;  
+                    }
+                }
+            }
+            else
+            {
+                basketVMs = new List<BasketVM>();
+            }
+            return basketVMs;
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _context.Categories.Include(c=>c.Children).Where(c=>c.IsMain && c.IsDeleted==false).ToListAsync();
+        }
+
+        public async Task<IDictionary<string, string>> GetSettings()
+        {
+            IDictionary<string,string> settings = await _context.Settings.ToDictionaryAsync(s=>s.Key,s=>s.Value);
+
+            return settings;
+        }
+    }
+}
---
+++ P133Allup/wwwroot/assets/js/custom.js
@@ -0,0 +1,67 @@
+﻿$(document).ready(function () {
+
+    $('.addToBasket').click(function myfunction(e) {
+        e.preventDefault();
+
+        let productId = $(this).data('id');
+
+        fetch('basket/AddBasket?id=' + productId)
+            .then(res => {
+                return res.text();
+            }).then(data => {
+                $('.header-cart').html(data)
+
+            })
+    })
+
+    $("#productModal").click(function (e) {
+        e.preventDefault();
+
+        let url = $(this).attr('href');
+        
+
+        fetch(url).then(res => {
+            return res.text();
+        })
+            .then(data => {
+                $('.modal-content').html(data);
+                $('.quick-view-image').slick({
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                    dots: false,
+                    fade: true,
+                    asNavFor: '.quick-view-thumb',
+                    speed: 400,
+                });
+
+                $('.quick-view-thumb').slick({
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    asNavFor: '.quick-view-image',
+                    dots: false,
+                    arrows: false,
+                    focusOnSelect: true,
+                    speed: 400,
+                });
+            })
+
+    })
+
+    $("#searchinput").keyup(function () {
+        let search = $(this).val();
+        let category = $("#category").find(":selected").val();
+
+        if (search.length >= 3) {
+            fetch('product/search?search=' + search + '&categoryId=' + category)
+                .then(res => {
+                    return res.text()
+                }).then(data => {
+                    $('#searchbody').html(data)
+                })
+        }
+        else {
+            $('#searchbody').html('')
+        }
+    })
+})
