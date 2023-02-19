2b5748ac0a5f8adb41baaba2ce1afaa375702929
SamadAliyev20 <samadaa@code.edu.az>
2023-02-20 02:11:08 +0400

    viewcomponents

---
+++ P133Allup/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P133Allup.DataAccessLayer;
+
+namespace P133Allup.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly AppDbContext _context;
+
+        public FooterViewComponent(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(IDictionary<string, string> settings)
+        {
+            return View(await Task.FromResult(settings));
+        }
+    }
+}
